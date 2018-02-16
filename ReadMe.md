f485082ef0606a655241f3ef2a49d2d154790009
RajvinderYogi <rajvinderyogi@yahoo.com>
2018-02-16 12:00:09 -0500

    added navigation and different pages in site and added read me file

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+<h1> Projects Porfolio- Assignment 1</h1>
+<h3> This website is built for Assignment-1(Advanced Server Side Scripting COMP 2106) at Georgian College</h3>
+<p>This website is abou my projects that I have done so far. I would use this website as a portfolio to show the employers.</p>
+<p>I am using CSS template from <a href="http://www.free-css.com/free-css-templates/page220/optics">Optics free Templates</a> to make my portfolio wbsite attractive</p>
+
+<small> Made By - Rajvinder Yogi - 200353207 - COMP2084</small>
--- controllers/index.js
+++ controllers/index.js
@@ -5,5 +5,17 @@ let router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home page', name: 'Rajvinder Singh Yogi', profession:'I am Web Developer and this is my portfolio in Node JS' });
 });
+router.get('/about-me', function(req, res, next) {
+    res.render('about-me', { title: 'About Me', name: 'Rajvinder Singh Yogi', profession:'I am Web Developer and this is my portfolio in Node JS' });
+});
+router.get('/services', function(req, res, next) {
+    res.render('services', { title: 'Services', profession:'I provide the services in Web Development and and Web Designing' });
+});
+router.get('/projects', function(req, res, next) {
+    res.render('projects', { title: 'Portfolio', profession:'Here is the list of projects I have done in Past' });
+});
+router.get('/contact-me', function(req, res, next) {
+    res.render('contact-me', { title: 'Contact',  profession:'The best way to contact me is to  fill out this form' });
+});
 
 module.exports = router;
