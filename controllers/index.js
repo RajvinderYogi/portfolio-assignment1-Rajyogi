499a31a0150f55b2ec8dd35b44ed71e42a0d2ae4
RajvinderYogi <rajvinderyogi@yahoo.com>
2018-02-17 16:20:19 -0500

    almost done with homepage and about me page

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,10 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home page', name: 'Rajvinder Singh Yogi', profession:'I am Web Developer and this is my portfolio in Node JS' });
+  res.render('index', { title: 'Home page', name: 'Rajvinder Singh Yogi', profession:'I am Web Developer/Designer and this is my portfolio website.' });
 });
 router.get('/about-me', function(req, res, next) {
-    res.render('about-me', { title: 'About Me', name: 'Rajvinder Singh Yogi', profession:'I am Web Developer and this is my portfolio in Node JS' });
+    res.render('about-me', { title: 'About Me', about :'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh ante facilisis bibendum.' });
 });
 router.get('/services', function(req, res, next) {
     res.render('services', { title: 'Services', profession:'I provide the services in Web Development and and Web Designing' });
