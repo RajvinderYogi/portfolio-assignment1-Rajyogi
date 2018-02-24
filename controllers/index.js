4c4968202367c54f9c05bab93e02075be5a0f23c
RajvinderYogi <rajvinderyogi@yahoo.com>
2018-02-24 16:32:41 -0500

    Content added in services page

--- controllers/index.js
+++ controllers/index.js
@@ -9,7 +9,7 @@ router.get('/about-me', function(req, res, next) {
     res.render('about-me', { title: 'About Me', about :'I Design and Develop responsive web application, I am pursuing my Post Graduate diploma in Interactive Design Media - Web from Georgian College of applied Arts - Barrie. I love what I do.' });
 });
 router.get('/services', function(req, res, next) {
-    res.render('services', { title: 'Services', profession:'I provide the services in Web Development and and Web Designing' });
+    res.render('services', { title: 'Services', profession:'I provide the services in Web Development and and Web Designing', service1: 'Web Designing', service1Body: 'Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque.', service2: 'Web Development', service2Body: 'Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque.', service3: 'Analytics', service3Body: 'Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque.', service4: 'Content Planning', service4Body: 'Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque.',service5: 'SEO/SEM', service5Body: 'Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque.',service6: 'Photography', service6Body: 'Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque.', section2:'Work Process'});
 });
 router.get('/projects', function(req, res, next) {
     res.render('projects', { title: 'Portfolio', profession:'Here is the list of projects I have done in Past' });
